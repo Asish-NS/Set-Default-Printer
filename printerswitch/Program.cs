d1f9f5af58234d81a7118442011aa608337baec1
Asish NS <85307440+Asish-NS@users.noreply.github.com>
2025-01-09 17:13:52 +0530

    Add files via upload

---
+++ printerswitch/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+using printerswitch;
+using System;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+//string deviceName = "Microsoft Print to PDF";
+
+string deviceName = "";
+
+foreach( string printername in args)
+{
+    deviceName = printername;
+}
+
+deviceName = deviceName.Replace("|"," ");
+
+bool isDefaultPrinter = IsPrinterDefault(deviceName);
+
+if (isDefaultPrinter)
+{
+    Console.WriteLine($"{deviceName} is the default printer.");
+}
+else
+{
+    if (PrinterExists(deviceName))
+    {
+        foreach (string printer in PrinterSettings.InstalledPrinters)
+        {
+            if (string.Equals(printer, deviceName, StringComparison.OrdinalIgnoreCase))
+            {
+                SetDefaultPrinterWMI(deviceName);
+                Console.WriteLine("Default Printer is SET");
+            }
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Printer Not Found");
+    }
+
+}
+
+static void SetDefaultPrinterWMI(string printerName)
+{
+    try
+    {
+        // WMI query to set default printer
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Printer");
+
+        foreach (ManagementObject printer in searcher.Get())
+        {
+            string currentPrinterName = printer["Name"].ToString();
+            if (currentPrinterName.Equals(printerName, StringComparison.OrdinalIgnoreCase))
+            {
+                printerclass.SetDefaultPrinter(printerName);             
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error setting default printer: {ex.Message}");
+    }
+}
+static bool IsPrinterDefault(string deviceName)
+{
+    // Get the current default printer
+    string defaultPrinter = new PrinterSettings().PrinterName;
+
+    return string.Equals(defaultPrinter, deviceName, StringComparison.OrdinalIgnoreCase);
+}
+static bool PrinterExists(string deviceName)
+{
+    // Get the list of installed printers
+    foreach (string printer in PrinterSettings.InstalledPrinters)
+    {
+        if (string.Equals(printer, deviceName, StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ printerswitch/printerclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printerswitch
+{
+    internal class printerclass
+    {
+        [DllImport("winspool.drv",
+             CharSet = CharSet.Auto,
+             SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern Boolean SetDefaultPrinter(String name);
+    }
+}
